0d6d772b1ecbdb50e60c339abff014e17449fd2d
Ram Bhawan Chauhan <rambhawan@Rams-MacBook-Pro.local>
2015-06-27 14:53:35 +0530

    Initial commit with contributors

---
+++ Classes/viewController/SettingsViewController.h
@@ -0,0 +1,28 @@
+//
+//  SettingsViewController.h
+//  QikAChat
+//
+//  Created by Ram Chauhan on 27/06/2015.
+//  Copyright (c) 2015 RAMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+extern NSString *const kXMPPmyJID;
+extern NSString *const kXMPPmyPassword;
+
+
+@interface SettingsViewController : UIViewController 
+{
+  UITextField *jidField;
+  UITextField *passwordField;
+}
+
+@property (nonatomic,strong) IBOutlet UITextField *jidField;
+@property (nonatomic,strong) IBOutlet UITextField *passwordField;
+
+- (IBAction)done:(id)sender;
+- (IBAction)hideKeyboard:(id)sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  QikAChat
+//
+//  Created by Ram Chauhan on 27/06/15.
+//  Copyright (c) 2015 RAMC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+   
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
